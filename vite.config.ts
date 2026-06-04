e749478b535121583d99cdae8a242fdf383a421a
Sambhav-gg <156508582+Sambhav-gg@users.noreply.github.com>
2026-06-04 17:52:26 +0530

    icons

--- vite.config.ts
+++ vite.config.ts
@@ -35,8 +35,8 @@ export default defineConfig({
         theme_color: '#6c63ff',
         orientation: 'portrait',
         icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
+          { src: '/public/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/public/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
         ],
       },
     }),
